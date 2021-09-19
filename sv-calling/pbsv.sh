ab5e2354be91b21ce4ae7bd0371809ae3a1aec8c
jijizhang <44020726+zhangjijin@users.noreply.github.com>
2021-09-19 15:30:13 +0800

    Update pbsv.sh

--- sv-calling/pbsv.sh
+++ sv-calling/pbsv.sh
@@ -1,4 +1,4 @@
 for i in pbmm ngmlr 
 do
-pbsv discover ${i}-2.bam ${i}-pbsv.svsig.gz && pbsv call ref.fa ${i}-pbsv.svsig.gz ${i}-pbsv.vcf --ccs 2> ${i}-pbsv.log 
+pbsv discover ${i}.bam ${i}-pbsv.svsig.gz && pbsv call ref.fa ${i}-pbsv.svsig.gz ${i}-pbsv.vcf --ccs 2> ${i}-pbsv.log 
 done
